09840ff5956e7766c967d7d3f3f361a67ce607b0
jn4248 <jnelson2@gmail.com>
2017-11-02 01:36:25 -0400

    Only 1 method to create the drawing grid, using grid display. other 2 methods are now broken. added function to prompt user for gridSize. Size of grid frame is static. no color button functionality. erase simply removes highlighted class.

--- scripts/main.js
+++ scripts/main.js
@@ -5,32 +5,140 @@ $(document).ready(function() {
   let mouseStillDown = false;
   let eraseOn = false;
 
-  const tileGridSize = 60;
 
+/////////////////////////////////////////////////////////////////////
+// asks user for a positive integer.  Checks, and repeats prompt until:
+// 1.  user enters positive integer:  returns the integer
+// 2.  user clicks 'cancel' on prompt, or enters an empty string:  returns '-1'
+function promptGridSize() {
+  // console.log('ran promptGridSize function');
+  let invalidInput = true;
+  let answer = prompt('Please enter the number of rows/columns, as a positive integer:', '10');
+  let gridSize = parseFloat(answer);
+  // console.log('size entered: ' + gridSize);
+  while (invalidInput) {
+    if ((answer === null) || (answer === "")) {
+      gridSize = -1;
+      invalidInput = false;
+      // console.log('prompt - canceled');
+      return gridSize;
+    } else if ((typeof gridSize === 'number') && (gridSize % 1 === 0) && (gridSize > 0)) {
+      invalidInput = false;
+      // console.log('prompt - good answer: ' + gridSize);
+      return gridSize;
+    } else {
+      // console.log('prompt - BAD answer: ' + gridSize);
+      answer = prompt('Incorrect format entered.  Please enter the number of rows/columns, as a positive integer:', '10');
+      gridSize = parseFloat(answer);
+      // console.log('new size entered: ' + gridSize);
+    }
+  }
+}
 
 /////////////////////////////////////////////////////////////////////
 
   // Build Grid system of tiles to draw with - Method 1
-  function createGrid1() {
-    //create grid, and a frame in which to center the grid
-    let grid1Frame = $('<div></div>').addClass('centered-content');
-    grid1Frame.hide();
-    let grid1 = $('<div></div').addClass('tiles-container');
-    grid1Frame.append(grid1);
-    $('#js-gridsContainer').append(grid1Frame);
-    // create the grid of tiles
-    for (let row = 0; row < tileGridSize; row++) {
-      for (let col = 0; col < tileGridSize; col++) {
-        let newTile = $('<div></div>').addClass('tile');
-        if (col === 0) {
-          newTile.addClass('left-tile');
+  function createGrid() {
+    //prompt user for number of rows/columns
+    let gridSize = promptGridSize();
+    //let gridSize = 80;
+
+    // create the drawing grid of tiles.
+    // Does not create grid if user entered an empty string, or canceled the prompt
+    if (gridSize > 0) {
+      let grid1 = $('<div></div>').addClass('tiles-container');
+      grid1.hide();
+      $('#js-gridContainer').append(grid1);
+
+      // set css variables for number of rows and columns
+      document.documentElement.style.setProperty("--numRows", gridSize);
+      document.documentElement.style.setProperty("--numColumns", gridSize);
+
+      // create the grid of tiles
+      let numTiles = gridSize * gridSize;
+      let newTile = $('<div></div>').addClass('tile');
+      for (let i = 0; i < numTiles; i++) {
+          grid1.append(newTile.clone());
+      }
+
+      grid1.fadeIn(100);
+      //grid1.show();
+    }
+  }
+
+
+  /////////////////////////////////////////////////////////////////////
+/*
+    // Build Grid system of tiles to draw with - Method 1
+    function createGrid1() {
+      //prompt user for number of rows/columns
+      let cancelGridCreation = false;
+      let waitingForQualifyingAnswer = true;
+      let answer = prompt('Please enter the number of rows/columns, as a positive integer:', '10');
+      let gridSize = parseFloat(answer, 10);
+      console.log('size entered: ' + gridSize);
+      while (waitingForQualifyingAnswer === true) {
+        if (answer == null || answer == "") {
+          cancelGridCreation = true;
+          console.log('prompt - canceled');
+          waitingForQualifyingAnswer = false;
+        } else if (typeof gridSize === 'number' && gridSize % 1 === 0 && gridSize > 0) {
+          waitingForQualifyingAnswer = false;
+          console.log('prompt - good answer: ' + gridSize);
+        } else {
+          console.log('prompt - BAD answer: ' + gridSize);
+          answer = prompt('Incorrect format entered.  Please enter the number of rows/columns, as a positive integer:', '16');
+          gridSize = parseFloat(answer, 10);
+          console.log('new size entered: ' + gridSize);
         }
-        grid1.append(newTile);
+      }
+
+      //create grid, and a frame in which to center the grid
+      // only create the grid if user did not cancel the prompt
+      if (cancelGridCreation === false) {
+        let grid1 = $('<div></div>').addClass('tiles-container grid1-id');
+        grid1.hide();
+        let gridsContainer = $('#js-gridsContainer');
+        gridsContainer.append(grid1);
+        // let gridHeight = gridsContainer.height();
+        // let tileHeight = gridHeight / gridSize;
+        // console.log('gridHeight is: ' + gridHeight);
+        // console.log('tileHeight is: ' + tileHeight);
+        // let gridWidth = gridsContainer.width();
+        // let tileWidth = gridWidth / gridSize;
+        // console.log('gridWidth is: ' + gridWidth);
+        // console.log('tileWidth is: ' + tileWidth);
+        // $('.tile').css({'height': 'tileHeight', 'width': 'tileWidth'});
+
+        // create the grid of tiles
+        for (let row = 0; row < gridSize; row++) {
+          for (let col = 0; col < gridSize; col++) {
+            let newTile = $('<div></div>').addClass('tile');
+            if (col === 0) {
+              newTile.addClass('left-tile');
+            }
+            grid1.append(newTile);
+          }
+        }
+        let gridHeight = grid1.height();
+        let tileHeight = gridHeight / gridSize;
+        console.log('gridHeight is: ' + gridHeight);
+        console.log('tileHeight is: ' + tileHeight);
+        let gridWidth = grid1.width();
+        let tileWidth = gridWidth / gridSize;
+        console.log('gridWidth is: ' + gridWidth);
+        console.log('tileWidth is: ' + tileWidth);
+        // $('.tile').css({'height': 'tileHeight', 'width': 'tileWidth'});
+        $('.tile').css('height', tileHeight);
+        $('.tile').css('width', tileWidth);
+
+
+        // grid1Frame.fadeIn(2000);
+        grid1.show();
       }
     }
-    // grid1Frame.fadeIn(2000);
-    grid1Frame.show();
-  }
+*/
+/////////////////////////////////////////////////////////////////////
 
   // Build Grid system of tiles to draw with - Method 2
   function createGrid2() {
@@ -39,7 +147,7 @@ $(document).ready(function() {
     grid2Frame.hide();
     let grid2 = $('<div></div').addClass('tiles-container');
     grid2Frame.append(grid2);
-    $('#js-gridsContainer').append(grid2Frame);
+    $('#js-gridContainer').append(grid2Frame);
     // create template row
     let rowTemplate = $('<div></div>').addClass('row-container');
     for (let i = 0; i < tileGridSize; i++) {
@@ -62,24 +170,24 @@ $(document).ready(function() {
 // note - no object available, for grids, to call fadeIn() on....PROBLEM
 
   // buttons to create the main drawing grid 1
-  $('#js-runGrid1Button').on('click', function() {
+  $('#js-runGridButton').on('click', function() {
     if ($(this).data('clicked') === false) {
-      createGrid1();
+      createGrid();
       $(this).data('clicked', 'true');
     } else {
-      alert('Grid 1 has already been displayed');
+      alert('Grid has already been displayed');
     }
   });
 
   // buttons to create the main drawing grid 1
-  $('#js-runGrid2Button').on('click', function() {
-    if ($(this).data('clicked') === false) {
-      createGrid2();
-      $(this).data('clicked', 'true');
-    } else {
-      alert('Grid 2 has already been displayed');
-    }
-  });
+  // $('#js-runGrid2Button').on('click', function() {
+  //   if ($(this).data('clicked') === false) {
+  //     createGrid2();
+  //     $(this).data('clicked', 'true');
+  //   } else {
+  //     alert('Grid 2 has already been displayed');
+  //   }
+  // });
 
   // button to toggle "erase" on/off
    $('#js-eraseButton').on('click', function() {
@@ -107,7 +215,7 @@ $(document).ready(function() {
 // stop mouseup/down PROBLEM
 
 // draw by either "highlighting" or "erasing" when clicking a tile
-$('#js-gridsContainer').on('mousedown', '.tile', function(event) {
+$('#js-gridContainer').on('mousedown', '.tile', function(event) {
   event.preventDefault();
   mouseStillDown = true;
   drawOnTile($(this));
@@ -119,15 +227,16 @@ $(document).on('mouseup', function(event) {
 });
 
 // option 1 - mouseenter instead of mousemove  (this one worked intially)
-$('#js-gridsContainer').on('mouseenter', '.tile', function(event) {
+$('#js-gridContainer').on('mouseenter', '.tile', function(event) {
   event.preventDefault();
   if (mouseStillDown) {
     drawOnTile($(this));
   }
 });
 
-// option 1 - mousemove instead of mouseenter (tried later...yet to try)
-// $('#js-gridsContainer').on('mousemove', '.tile', function(event) {
+// option 2 - mousemove instead of mouseenter
+// (ttried: was equally responsive as mouseenter - still skipped with fast mouse movement)
+// $('#js-gridContainer').on('mousemove', '.tile', function(event) {
 //   event.preventDefault();
 //   if (mouseStillDown) {
 //     drawOnTile($(this));
